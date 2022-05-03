99c06abd1dce923da707f0e1406bd86f0704da19
granadalook <tiven17.jsga@gmail.com>
2022-05-03 14:22:41 -0500

    CREACION: se crean  todas las peticiones  en TodoController  y se corre la aplicacion sin errores

--- crud-backend-springboot/src/main/java/com/example/demo/TodoController.java
+++ crud-backend-springboot/src/main/java/com/example/demo/TodoController.java
@@ -1,8 +1,40 @@
 package com.example.demo;
 
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class TodoController {
+    @Autowired
+    private TodoService service;
 
+    @GetMapping(path = "api/todos")
+    public Iterable<Todo> list() {
+        return service.list();
+    }
+
+    @PostMapping(path = "api/todo")
+    public Todo save(@RequestBody Todo todo) {
+        return service.save(todo);
+    }
+
+    @PutMapping(path = "api/todo")
+    public Todo update(@RequestBody Todo todo) {
+        if (todo.getId() != 0) {
+            return service.save(todo);
+        }
+        throw new RuntimeException("no exisate");
+    }
+
+    @DeleteMapping(path = "api/{id}/todo")
+    public void delete(@PathVariable("id") Long id) {
+        service.delete(id);
+
+    }
+
+    @GetMapping(path = "api/{id}/todo")
+    public Todo get(@PathVariable("id") Long id) {
+        return service.get(id);
+
+    }
 }
